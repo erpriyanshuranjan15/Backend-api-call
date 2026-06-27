81dd098117c7ff45996d465f0b3620aa5d9afe41
erpriyanshuranjan15 <er.priyanshuranjan0515@gmail.com>
2026-06-27 07:24:07 +0530

    updated gitignore

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./src/app.js";
-import connectDB from "./src/db.js"
+import connectDB from "./src/database/db.js"
 connectDB()
 app.listen(3000,()=>{
     console.log ("Server is running on 3000")
